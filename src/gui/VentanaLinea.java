c937f1f5ef1264763bc32710f62222e8c808dfec
Carsopre <cssp1989@gmail.com>
2013-04-06 13:10:23 +0200

    no la liem

--- src/gui/VentanaLinea.java
+++ src/gui/VentanaLinea.java
@@ -12,8 +12,11 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTextField;
-
+import javax.swing.SpinnerListModel;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.WindowConstants;
 import javax.swing.SwingUtilities;
 
